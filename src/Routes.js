22096970d2e07ea9af0fd4fb7c664d3ca76fb6ac
obemb <peterihaza@gmail.com>
2023-08-22 15:12:11 +0100

    makolaa

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './Page/Login'
+import Landing from './Page/Landing'
+import repaymentSchedule from './Page/RepaymentBreakdown'
+
+const route =  [
+    // { path: "/Login" ,  Component: Login},
+    { path: "/" ,  Component: Landing},
+    // { path: "/repaymentSchedule" ,  Component: repaymentSchedule},
+  ]
+
+export default route;
